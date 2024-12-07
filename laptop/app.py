719f89d62b776a6cdb6f08d558806c6952925224
Christopher Barbato <barbatocg@hotmail.com>
2024-12-06 17:40:36 -0800

    final commit

--- laptop/app.py
+++ laptop/app.py
@@ -1,17 +1,20 @@
 import flask
-from flask import request, jsonify, send_from_directory
+from flask import request, jsonify, send_from_directory, url_for
 from flask_restful import Api, Resource
 import config
 from pymongo import MongoClient, errors
 import os
 import logging
+from werkzeug.utils import secure_filename
 
 app = flask.Flask(__name__)
 api = Api(app)
 CONFIG = config.configuration()
 app.secret_key = CONFIG.SECRET_KEY
+boulders = []
 
-UPLOAD_FOLDER = 'static/uploads' #temp dir to hold uploaded files
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static/uploads') #temp dir to hold uploaded files
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -19,7 +22,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route("/index", methods=['GET'])
 def index():
     app.logger.debug("Main page entry")
-    return flask.render_template('main.html')
+    return flask.render_template('index.html')
 
 @app.route("/my_boulders", methods=["GET"])
 def my_boulders():
@@ -32,38 +35,49 @@ def submit():
     print("files received:", request.files)
     name = request.form.get('boulder_name')
     grade = request.form.get('boulder_grade')
-    media_file = request.files.get('boulder-media')
+    media_file = request.files.get('boulder_media')
+    if not media_file:
+        print('no media file found in request')
 
     if not name or not grade:
         return jsonify({"error": "Missing required fields"}), 400
 
     media_url = None
 
-    if media_file:
-        media_filename = os.path.join(UPLOAD_FOLDER, media_file.filename)
-        media_path = os.path.join(app.config['UPLOAD_FOLDER'], media_filename)
-        print(f"saving file to {media_path}")
-        media_file.save(media_path)
+    filename = secure_filename(media_file.filename)
 
-        if os.path.exists(media_path):
-            print(f"File saved successfully: {media_path}")
-        else:
-            print(f"File save failed: {media_path}")
-        media_url = f'/uploads/{media_filename}'
-        print(f"generated media url: {media_url}")
+    if media_file:
+        #media_filename = os.path.join(UPLOAD_FOLDER, media_file.filename)
+        media_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        print(f"saving file to {media_path}")
+
+        try:
+            media_file.save(media_path)
+            print(f'file saved successfully: {media_path}')
+        except Exception as e:
+            print(f'error saving file: {e}')
+            return 'file save failed', 500
+    
+    media_url = url_for('uploaded_file', filename=filename)
 
     response_data = {
+        'id': len(boulders) + 1,
         'name': name,
         'grade': grade,
-        'mediaUrl': media_url
+        'media_url': media_url
         }
+    boulders.append(response_data)
     
-    return jsonify(response_data), 200
-    #return flask.redirect(flask.url_for('index'))
-
+    #return jsonify(response_data), 200
+    return flask.redirect(url_for('index'))
+    
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/boulders', methods=['GET'])
+def show_boulders():
+    return flask.render_template('boulders.html', boulders=boulders)
 
 app.debug = True
 if app.debug:
